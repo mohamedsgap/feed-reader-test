aaa4f9bf22650d54d69c19704ad294a32dba8ea5
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-03-21 19:40:27 +0200

    edit some tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -21,7 +21,7 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
-        it('are defined', function() {
+        it('allFeeds variables are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
@@ -31,7 +31,7 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-        it('allFeeds has URL & not Empty', function() {
+        it('allFeeds haave URL & not Empty', function() {
             for (feed of allFeeds){
                 expect(feed["url"]).toBeDefined();
                 expect(feed["url"]).not.toBe('');
@@ -43,7 +43,7 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-        it('allFeeds has Name & not Empty', function() {
+        it('allFeeds haave Name & not Empty', function() {
             for (feed of allFeeds){
                 expect(feed['name']).toBeDefined();
                 expect(feed['name']).not.toBe('');
